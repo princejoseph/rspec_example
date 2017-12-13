75da537b6da028d4b05c7c2e1d11487bdfec20a4
Prince <prince@mobomo.com>
2017-12-13 11:03:38 +0530

    Moving car to a let to reuse in specs.

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,14 +1,14 @@
 require 'car'
 
 RSpec.describe Car do
+  let(:car) { Car.new('KL 22 E 4177', 'White') }
+
   describe '#having_colour?' do
     it 'gives true for the same colour' do
-      car = Car.new('KL 22 E 4177', 'White')
       expect(car.having_colour?('White')).to eq(true)
     end
 
     it 'gives false for different colour' do
-      car = Car.new('KL 22 E 4177', 'White')
       expect(car.having_colour?('Red')).to eq(false)
     end
 
